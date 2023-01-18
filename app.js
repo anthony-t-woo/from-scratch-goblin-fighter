c29fbcb2528ecd85337eccb67cd14131c8743c4e
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-18 12:30:39 -0700

    added feature to prompt user of game over

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const nameButton = document.getElementById('name-button');
 const goblinsEl = document.querySelector('.goblins-container');
 const playerHealthEl = document.getElementById('player-hp');
 /* State */
-let playerHP = 10;
+let playerHP = 5;
 // Starter Goblins
 const goblins = [
     { name: 'Drew', health: 4 },
@@ -24,6 +24,10 @@ function displayGoblins() {
     for (let goblin of goblins) {
         const goblinEl = renderDiv(goblin);
         goblinEl.addEventListener('click', () => {
+            if (playerHP < 1) {
+                alert('Player has succumbed to the goblin attacks and can not go on fighting');
+                return;
+            }
             if (Math.random() > 0.5) {
                 goblin.health--;
                 alert(`Your attack on ${goblin.name} was successful`);
