f2417eaaa4d307e49267369e832e8bdd736611ce
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-18 11:53:08 -0700

    render and display goblin divs from starter list is functional

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 /* Imports */
-
+import { renderDiv } from './render-utils.js';
 /* Get DOM Elements */
 const goblinsKilled = document.getElementById('goblins-defeated');
 const nameInput = document.getElementById('name-input');
 const nameButton = document.getElementById('name-button');
-
+const goblinsEl = document.querySelector('.goblins-container');
 /* State */
 let playerHP = 10;
 // Starter Goblins
 const goblins = [
-    { name: Drew, health: 4 },
-    { name: Korbin, health: 3 },
-    { name: Lisa, health: 4 },
-    { name: Dave, health: 2 },
+    { name: 'Drew', health: 4 },
+    { name: 'Korbin', health: 3 },
+    { name: 'Lisa', health: 4 },
+    { name: 'Dave', health: 2 },
 ];
 
 /* Events */
 
 /* Display Functions */
+function displayGoblins() {
+    goblinsEl.textContent = '';
+    for (let goblin of goblins) {
+        const goblinEl = renderDiv(goblin);
+        goblinsEl.append(goblinEl);
+    }
+    console.log(goblinsEl);
+}
 
 // (don't forget to call any display functions you want to run on page load!)
+displayGoblins();
---
+++ render-utils.js
@@ -0,0 +1,20 @@
+export function renderDiv(object) {
+    const div = document.createElement('div');
+    const nameEl = document.createElement('p');
+    const emojiEl = document.createElement('p');
+    const healthEl = document.createElement('p');
+
+    nameEl.textContent = object.name;
+    healthEl.textContent = object.health;
+
+    div.classList.add('goblin');
+
+    if (object.health > 1) {
+        emojiEl.textContent = '😈';
+    } else {
+        emojiEl.textContent = '💀';
+    }
+
+    div.append(nameEl, emojiEl, healthEl);
+    return div;
+}
