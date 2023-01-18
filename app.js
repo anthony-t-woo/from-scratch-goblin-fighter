f431f15fa38487391e55b1020c5dac2865d75cf4
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-18 11:10:11 -0700

    JS get DOM and initialize state

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 /* Imports */
 
 /* Get DOM Elements */
+const goblinsKilled = document.getElementById('goblins-defeated');
+const nameInput = document.getElementById('name-input');
+const nameButton = document.getElementById('name-button');
 
 /* State */
+let playerHP = 10;
+// Starter Goblins
+const goblins = [
+    { name: Drew, health: 4 },
+    { name: Korbin, health: 3 },
+    { name: Lisa, health: 4 },
+    { name: Dave, health: 2 },
+];
 
 /* Events */
 
